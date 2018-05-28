0a7034e5b8d0130ee7a944ab3920a8b2c9be01b3
ldps21 <33166069+ldps21@users.noreply.github.com>
2018-05-28 15:34:14 -0400

    Dagger2 implemented
    Dagger 2 added to project

--- app/src/main/java/com/iitp/njack/iitp_connect/core/androidapp/IITPConnectApplication.java
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/androidapp/IITPConnectApplication.java
@@ -2,12 +2,34 @@ package com.iitp.njack.iitp_connect.core.androidapp;
 
 import android.app.Application;
 
+import com.iitp.njack.iitp_connect.core.Injection.Component.ApplicationComponent;
+import com.iitp.njack.iitp_connect.core.Injection.Component.DaggerApplicationComponent;
+import com.iitp.njack.iitp_connect.core.Injection.Module.ApplicationModule;
+
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerApplication;
+import retrofit2.Retrofit;
 import timber.log.Timber;
 
 public class IITPConnectApplication extends Application {
+
+    private ApplicationComponent applicationComponent;
+
+    private static IITPConnectApplication singleton;
+
+    public static IITPConnectApplication getInstance(){ return singleton;}
+
     @Override
     public void onCreate() {
         super.onCreate();
         Timber.plant(new Timber.DebugTree());
+        applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+        applicationComponent.Inject(this);
+        singleton = this;
+
+
     }
 }
