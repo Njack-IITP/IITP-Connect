bfcee87dc60ee525cb7eeeba15f7e86a7115ef42
Saurav Tiwary <srv.twry@gmail.com>
2018-10-18 16:20:05 +0530

    fix: Use Lambdas, Remove unused imports (#165)

--- app/src/main/java/com/iitp/njack/iitp_connect/core/profile/ProfileActivity.java
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/profile/ProfileActivity.java
@@ -4,16 +4,12 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.iitp.njack.iitp_connect.R;
 import com.iitp.njack.iitp_connect.data.user.User;
 import com.iitp.njack.iitp_connect.databinding.ActivityProfileBinding;
