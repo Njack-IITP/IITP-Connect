58395a96c0b4f3fa7d2a8d0c7737e358abcc0250
Sheetal Gupta <sheetalgupta000001@gmail.com>
2018-12-14 16:20:40 +0530

    add lombok for for constructor getter and setter

--- app/src/main/java/com/iitp/njack/iitp_connect/core/youtube/models/YoutubePlaylist.java
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/youtube/models/YoutubePlaylist.java
@@ -2,42 +2,16 @@ package com.iitp.njack.iitp_connect.core.youtube.models;
 
 import com.google.api.client.util.DateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class YoutubePlaylist {
-    private DateTime publishedAt;
-    private String playlist_id;
-    private String title;
-    private long itemCount;
-    private String date;
-
-    public YoutubePlaylist(String playlist_id) {
-        this.playlist_id = playlist_id;
-    }
-
-    public YoutubePlaylist(String playlist_id, String title, long itemCount, DateTime publishedAt) {
-        this.playlist_id = playlist_id;
-        this.title = title;
-        this.itemCount = itemCount;
-        this.publishedAt = publishedAt;
-        this.date = publishedAt.toStringRfc3339();
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public DateTime getPublishedAt() {
-        return publishedAt;
-    }
-
-    public String getPlaylistId() {
-        return playlist_id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public long getItemCount() {
-        return itemCount;
-    }
+    String playlist_id;
+    String title;
+    long itemCount;
+    DateTime publishedAt;
 }
--- app/src/main/java/com/iitp/njack/iitp_connect/core/youtube/models/YoutubeVideo.java
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/youtube/models/YoutubeVideo.java
@@ -2,33 +2,14 @@ package com.iitp.njack.iitp_connect.core.youtube.models;
 
 import com.google.api.client.util.DateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class YoutubeVideo {
     private String video_id;
     private String title;
     private DateTime publishedAt;
     private String imageUrl;
-
-    public YoutubeVideo(String video_id, String title, DateTime publishedAt,String thumbnail) {
-        this.video_id = video_id;
-        this.title = title;
-        this.publishedAt = publishedAt;
-        this.imageUrl = thumbnail;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getPublishedAt() {
-        return publishedAt.toString().substring(0, 10);
-    }
-
-    public String getVideoId() {
-        return video_id;
-    }
-
 }
