75f30eb7b1ba2ecd47c5dfe54d2418072ead6186
ravi-kishan <31040431+ravi-kishan@users.noreply.github.com>
2018-12-24 17:19:48 +0530

    feat: Add FB feed model (#226)
    * Added facev=bookfeedview
    
    * Added facev=bookfeedviewholder
    
    * Added facev=bookfeedviewmodel
    
    * Added facebookfeedpostlayout
    
    * Added some colors
    
    * Added like image
    
    * Added like vector
    
    * Added comment vector
    
    * Added share vector
    
    * Made formatting changes
    
    * Made scope change and gradle update
    
    * Undo gradle change
    
    * Removed unnecessary new lines
    
    * Glide with injection
    
    * Removed empty lines

---
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/facebook/feed/FacebookFeedView.java
@@ -0,0 +1,11 @@
+package com.iitp.njack.iitp_connect.core.facebook.feed;
+
+import com.iitp.njack.iitp_connect.common.view.Emptiable;
+import com.iitp.njack.iitp_connect.common.view.Erroneous;
+import com.iitp.njack.iitp_connect.common.view.Progressive;
+import com.iitp.njack.iitp_connect.common.view.Refreshable;
+import com.iitp.njack.iitp_connect.data.facebook.FacebookPost;
+
+public interface FacebookFeedView extends Progressive, Erroneous, Refreshable, Emptiable<FacebookPost> {
+    void openFacebookPostDetails(Long id);
+}
