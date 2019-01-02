de2dcb1893016c59a89e297bfb1036aab507f72c
Sheetal Gupta <sheetalgupta000001@gmail.com>
2019-01-02 15:50:34 +0530

    add dao class for playlists

---
+++ app/src/main/java/com/iitp/njack/iitp_connect/data/youtube/PlaylistDao.java
@@ -0,0 +1,26 @@
+package com.iitp.njack.iitp_connect.data.youtube;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+
+import java.util.List;
+
+@Dao
+public interface PlaylistDao {
+    @Query("SELECT * FROM " + YoutubePlaylist.TABLE_NAME)
+    LiveData<List<YoutubePlaylist>> getPlaylists();
+
+    @Query("SELECT * FROM " + YoutubePlaylist.TABLE_NAME + " WHERE channelId=:id")
+    LiveData<List<YoutubePlaylist>> getPlaylistsById(String id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void addPlayists(List<YoutubePlaylist> playlists);
+
+    @Query("DELETE FROM " + YoutubePlaylist.TABLE_NAME)
+    void deleteAll();
+}
