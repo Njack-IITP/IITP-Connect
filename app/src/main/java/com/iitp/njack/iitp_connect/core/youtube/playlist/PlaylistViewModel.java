b16072691ba049fefe7b6239deac450142ae0ba5
Sheetal Gupta <sheetalgupta000001@gmail.com>
2019-01-16 16:45:06 +0530

    add playlistViewModel

---
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/youtube/playlist/PlaylistViewModel.java
@@ -0,0 +1,64 @@
+package com.iitp.njack.iitp_connect.core.youtube.playlist;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableInt;
+import android.view.View;
+
+import com.iitp.njack.iitp_connect.R;
+import com.iitp.njack.iitp_connect.data.youtube.YoutubePlaylist;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class PlaylistViewModel extends ViewModel {
+    public ObservableInt loading;
+    public ObservableInt showEmpty;
+    private MutableLiveData<YoutubePlaylist> selected;
+    private PlaylistRepository playlistRepository;
+    private PlaylistAdapter playlistAdapter;
+
+    @Inject
+    public PlaylistViewModel(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+        selected = new MutableLiveData<>();
+        playlistAdapter = new PlaylistAdapter(R.layout.view_playlist, this);
+        loading = new ObservableInt(View.GONE);
+        showEmpty = new ObservableInt(View.GONE);
+    }
+
+    public void fetchList(String channelId) {
+        playlistRepository.getDataFromAPI(channelId);
+    }
+
+    public void onItemClick(Integer index) {
+        selected.setValue(getYoutubePlaylistAt(index));
+    }
+
+    public MutableLiveData<List<YoutubePlaylist>> getPlaylists() {
+        return playlistRepository.getPlaylists();
+    }
+
+    public YoutubePlaylist getYoutubePlaylistAt(Integer index) {
+        if (playlistRepository.getPlaylists().getValue() != null &&
+            index != null &&
+            playlistRepository.getPlaylists().getValue().size() > index) {
+            return playlistRepository.getPlaylists().getValue().get(index);
+        }
+        return null;
+    }
+
+    public PlaylistAdapter getAdapter() {
+        return playlistAdapter;
+    }
+
+    public void setPlaylistsInAdapter(List<YoutubePlaylist> playlists) {
+        this.playlistAdapter.setPlaylists(playlists);
+        this.playlistAdapter.notifyDataSetChanged();
+    }
+
+    public MutableLiveData<YoutubePlaylist> getSelected() {
+        return this.selected;
+    }
+}
