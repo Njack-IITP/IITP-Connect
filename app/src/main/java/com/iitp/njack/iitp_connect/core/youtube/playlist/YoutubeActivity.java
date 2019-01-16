f7a60c5923576c34e26f159b9989ff3175788203
Sheetal Gupta <sheetalgupta000001@gmail.com>
2019-01-16 16:47:47 +0530

    add acticity to display playlists

---
+++ app/src/main/java/com/iitp/njack/iitp_connect/core/youtube/playlist/YoutubeActivity.java
@@ -0,0 +1,215 @@
+package com.iitp.njack.iitp_connect.core.youtube.playlist;
+
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.services.youtube.YouTubeScopes;
+import com.iitp.njack.iitp_connect.R;
+import com.iitp.njack.iitp_connect.databinding.ActivityYoutubeBinding;
+
+import javax.inject.Inject;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class YoutubeActivity extends AppCompatActivity {
+    private static final int REQUEST_ACCOUNT_PICKER = 1000;
+    private static final int REQUEST_AUTHORIZATION = 1001;
+    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    private static final String[] SCOPES = {YouTubeScopes.YOUTUBE_READONLY};
+    @Inject
+    GoogleAccountCredential googleAccountCredential;
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+    private SwipeRefreshLayout refreshLayout;
+    private PlaylistViewModel playlistViewModel;
+    private ActivityYoutubeBinding binding;
+    private String channelId ="UC_x5XG1OV2P6uZZ5FSM9Ttw";// "UCWhU42H5JBpH49KtoWt2ZpQ";//
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        channelId = getIntent().getStringExtra("channelId") == null ? "UC_x5XG1OV2P6uZZ5FSM9Ttw" :getIntent().getStringExtra("channelId");
+        setupbindings(savedInstanceState);
+    }
+
+    private void setupbindings(Bundle savedInstanceState) {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_youtube);
+        playlistViewModel = ViewModelProviders.of(this, viewModelFactory).get(PlaylistViewModel.class);
+        getResultsFromApi();
+        binding.setModel(playlistViewModel);
+        setUpListUpdate();
+    }
+
+    private void setUpListUpdate() {
+        playlistViewModel.loading.set(View.VISIBLE);
+        playlistViewModel.fetchList(channelId);
+        playlistViewModel.getPlaylists().observe(this, youtubePlaylists -> {
+            playlistViewModel.loading.set(View.GONE);
+            if (youtubePlaylists.size() == 0) {
+                playlistViewModel.showEmpty.set(View.VISIBLE);
+            } else {
+                playlistViewModel.showEmpty.set(View.GONE);
+                playlistViewModel.setPlaylistsInAdapter(youtubePlaylists);
+            }
+        });
+        setUpRefreshListener();
+        setUpListClick();
+    }
+
+    private void setUpListClick() {
+        playlistViewModel.getSelected().observe(this, youtubePlaylist -> {
+            if (youtubePlaylist != null) {
+                Toast.makeText(YoutubeActivity.this, "you selected a " + youtubePlaylist.getPlaylistId(), Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(YoutubeActivity.this, VideoActivity.class);
+//                intent.putExtra("playlist_id", youtubePlaylist.getPlaylistId());
+//                startActivity(intent);
+            }
+        });
+    }
+
+    private void setUpRefreshListener() {
+        refreshLayout = binding.swipeContainer;
+        refreshLayout.setOnRefreshListener(() -> {
+            refreshLayout.setRefreshing(false);
+            playlistViewModel.fetchList(channelId);
+            Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void getResultsFromApi() {
+        if (!isGooglePlayServicesAvailable()) {
+            acquireGooglePlayServices();
+        } else if (googleAccountCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else if (!isDeviceOnline()) {
+            Log.e("network error", "No network connection available.");
+        }
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccount() {
+        if (EasyPermissions.hasPermissions(
+            this, Manifest.permission.GET_ACCOUNTS)) {
+            String accountName = getPreferences(Context.MODE_PRIVATE)
+                .getString(PREF_ACCOUNT_NAME, null);
+            if (accountName != null) {
+                googleAccountCredential.setSelectedAccountName(accountName);
+                getResultsFromApi();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                    googleAccountCredential.newChooseAccountIntent(),
+                    REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs to access your Google account (via Contacts).",
+                REQUEST_PERMISSION_GET_ACCOUNTS,
+                Manifest.permission.GET_ACCOUNTS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(
+        int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    Log.e("google", "google play services not found");
+                } else {
+                    getResultsFromApi();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                    data.getExtras() != null) {
+                    String accountName =
+                        data.getStringExtra(
+                            AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings =
+                            getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        googleAccountCredential.setSelectedAccountName(accountName);
+                        getResultsFromApi();
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    getResultsFromApi();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+            requestCode, permissions, grantResults, this);
+    }
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+            (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability =
+            GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+            apiAvailability.isGooglePlayServicesAvailable(this);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability =
+            GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+            apiAvailability.isGooglePlayServicesAvailable(this);
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    private void showGooglePlayServicesAvailabilityErrorDialog(
+        final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+            YoutubeActivity.this,
+            connectionStatusCode,
+            REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+}
